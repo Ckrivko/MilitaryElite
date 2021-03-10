87fe7442eb56119df5a0f501f75a478ded983b15
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-03-10 10:37:47 +0200

    Add files via upload

---
+++ Contracts/IMission.cs
@@ -0,0 +1,17 @@
+﻿using MilitaryElite.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Contracts
+{
+    public interface IMission
+    {
+        string CodeName { get; }
+
+        MissionState MissionState { get; }
+
+        void CompleteMission();
+
+    }
+}
